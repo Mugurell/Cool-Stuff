340f48f75354732da90a36f94422f14f7f79a417
Mugurel <petrumugurel@gmail.com>
2015-08-24 18:56:14 +0300

    Tic Tac Toe - WIP
    Need to add AI and maybe some cosmetic changes

---
+++ WF - Tic Tac Toe/WF - Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace WF___Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.A1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rulesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resetScoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.draws_score_box = new System.Windows.Forms.Label();
+            this.user_score_box = new System.Windows.Forms.Label();
+            this.ai_score_box = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // A1
+            // 
+            resources.ApplyResources(this.A1, "A1");
+            this.A1.Name = "A1";
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button_click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.resetScoresToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            resources.ApplyResources(this.newGameToolStripMenuItem, "newGameToolStripMenuItem");
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rulesToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            resources.ApplyResources(this.helpToolStripMenuItem, "helpToolStripMenuItem");
+            // 
+            // rulesToolStripMenuItem
+            // 
+            this.rulesToolStripMenuItem.Name = "rulesToolStripMenuItem";
+            resources.ApplyResources(this.rulesToolStripMenuItem, "rulesToolStripMenuItem");
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            resources.ApplyResources(this.aboutToolStripMenuItem, "aboutToolStripMenuItem");
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // A2
+            // 
+            resources.ApplyResources(this.A2, "A2");
+            this.A2.Name = "A2";
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button_click);
+            // 
+            // A3
+            // 
+            resources.ApplyResources(this.A3, "A3");
+            this.A3.Name = "A3";
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button_click);
+            // 
+            // B1
+            // 
+            resources.ApplyResources(this.B1, "B1");
+            this.B1.Name = "B1";
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button_click);
+            // 
+            // B2
+            // 
+            resources.ApplyResources(this.B2, "B2");
+            this.B2.Name = "B2";
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button_click);
+            // 
+            // B3
+            // 
+            resources.ApplyResources(this.B3, "B3");
+            this.B3.Name = "B3";
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button_click);
+            // 
+            // C1
+            // 
+            resources.ApplyResources(this.C1, "C1");
+            this.C1.Name = "C1";
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button_click);
+            // 
+            // C2
+            // 
+            resources.ApplyResources(this.C2, "C2");
+            this.C2.Name = "C2";
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button_click);
+            // 
+            // C3
+            // 
+            resources.ApplyResources(this.C3, "C3");
+            this.C3.Name = "C3";
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button_click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // resetScoresToolStripMenuItem
+            // 
+            this.resetScoresToolStripMenuItem.Name = "resetScoresToolStripMenuItem";
+            resources.ApplyResources(this.resetScoresToolStripMenuItem, "resetScoresToolStripMenuItem");
+            this.resetScoresToolStripMenuItem.Click += new System.EventHandler(this.resetScoresToolStripMenuItem_Click);
+            // 
+            // draws_score_box
+            // 
+            resources.ApplyResources(this.draws_score_box, "draws_score_box");
+            this.draws_score_box.Name = "draws_score_box";
+            // 
+            // user_score_box
+            // 
+            resources.ApplyResources(this.user_score_box, "user_score_box");
+            this.user_score_box.Name = "user_score_box";
+            this.user_score_box.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // ai_score_box
+            // 
+            resources.ApplyResources(this.ai_score_box, "ai_score_box");
+            this.ai_score_box.Name = "ai_score_box";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ai_score_box);
+            this.Controls.Add(this.user_score_box);
+            this.Controls.Add(this.draws_score_box);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rulesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem resetScoresToolStripMenuItem;
+        private System.Windows.Forms.Label draws_score_box;
+        private System.Windows.Forms.Label user_score_box;
+        private System.Windows.Forms.Label ai_score_box;
+    }
+}
+
---
+++ WF - Tic Tac Toe/WF - Tic Tac Toe/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF___Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        private bool user_turn = true;
+        private int turn_count = 0;
+        private int user_score = 0;
+        private int ai_score = 0;
+        private int draws = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("By Lingurar Petru-Mugurel\n\n\nCheck sources on Github?",
+                "About TTT", MessageBoxButtons.OKCancel)
+                == System.Windows.Forms.DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start("www.goagal.ro");
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to exit?", "Warning",
+                MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (user_turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            // change the turn
+            user_turn = !user_turn;
+
+            // prevent the user to change the recorded state
+            b.Enabled = false;
+
+            // for every move increase the turn count
+            ++turn_count;
+
+            // after any player could have made 3 moved check for a winner
+            if (turn_count >= 5)
+                winner_check();
+
+        }
+
+        private void winner_check()
+        {
+            bool winner = false;
+
+            /**
+             * The third check is to make sure all buttons on the line checked
+             * contain X or 0. 
+             * If one is disabled - was used, and all are the same,
+             * all are disabled - all contain X or 0.
+             */
+
+            // horizontal lines
+            if ((A1.Text == A2.Text) && (A1.Text == A3.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((B1.Text == B2.Text) && (B1.Text == B3.Text) && (!B1.Enabled))
+                winner = true;
+            else if ((C1.Text == C2.Text) && (C1.Text == C3.Text) && (!C1.Enabled))
+                winner = true;
+
+            // vertical lines
+            else if ((A1.Text == B1.Text) && (A1.Text == C1.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((A2.Text == B2.Text) && (A2.Text == C2.Text) && (!A2.Enabled))
+                winner = true;
+            else if ((A3.Text == B3.Text) && (A3.Text == C3.Text) && (!A3.Enabled))
+                winner = true;
+
+            // diagonal lines
+            else if ((A1.Text == B2.Text) && (A1.Text == C3.Text) && (!A1.Enabled))
+                winner = true;
+            else if ((A3.Text == B2.Text) && (A3.Text == C1.Text) && (!A3.Enabled))
+                winner = true;
+
+
+            if (winner)
+            {
+                // make sure the user can't continue to play
+                disable_buttons();
+
+                if (user_turn)
+                {
+                    ai_score_box.Text = (++ai_score).ToString();
+                    if (MessageBox.Show("You lost.\nWas expected really...",
+                        "Haha Ha!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        resetButtonsAndTurns();
+                    }
+                }
+                else
+                {
+                    user_score_box.Text = (++user_score).ToString();
+                    if (MessageBox.Show("You won??\n\n\nWanna test you luck again?",
+                        "Hmmm", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        resetButtonsAndTurns();
+                    }
+                }
+            }
+            else
+            {
+                if (turn_count == 9)
+                {
+                    draws_score_box.Text = (++draws).ToString();
+                    if (MessageBox.Show("It is a draw!\nYou were a worthy adversary!\n\n\nTry again?",
+                        "Congrats!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                    {
+                        resetButtonsAndTurns();
+                    }
+                }
+            }
+        }
+
+        // disable all buttons
+        private void disable_buttons()
+        {
+            /**
+             * Use a try catch to prevent 
+             * "Unable to cast object of type 'System.Windows.Forms.Label' 
+             * to type 'System.Windows.Forms.Button'. "
+             */
+
+            foreach (Control c in Controls)
+            {
+                // will fail when trying to convert a label to a button
+                // but will continue trying to do the rest of the controls
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+                catch {  /* ignored*/ }
+            }
+        }
+
+        // will reset all our buttons, the turn_count and who's turn it is
+        // the scores will remain intact
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("This will end your current game!", "Warning",
+                MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                resetButtonsAndTurns();
+            }               
+        }
+
+
+        private void resetButtonsAndTurns()
+        {
+            user_turn = true;       // always the user will be first
+            turn_count = 0;
+
+            foreach (Control c in Controls)
+            {
+                // will fail when trying to convert a label to a button
+                // but will continue trying to do the rest of the controls
+                try
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;   // make sure all buttons are enabled
+                    b.Text = "";        // each will contain blank
+                }
+                catch { /* ignored*/ }
+            }
+        }
+
+
+        private void resetScoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // print a warning message and let the user choose if he really
+            // wants to end the current game and reset all scores
+            if (MessageBox.Show("This will end your current game!", "Warning", 
+                MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                // reset all buttons, turn_count and who's turn is it
+                resetButtonsAndTurns();
+
+                // reset all scores and make them visible to the user
+                user_score = ai_score = draws = 0;
+                user_score_box.Text = (user_score).ToString();
+                ai_score_box.Text = (ai_score).ToString();
+                draws_score_box.Text = (draws).ToString();
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
